328eae61329a304703404c80a1f7381a874f601a
supunVihanga <73782644+supunVihanga@users.noreply.github.com>
2022-06-12 15:07:07 +0530

    creating method
    creating a method for getting all the permission list

--- elms/src/main/java/lk/ac/vau/elms/Controller/PermissionController.java
+++ elms/src/main/java/lk/ac/vau/elms/Controller/PermissionController.java
@@ -1,7 +1,24 @@
 package lk.ac.vau.elms.Controller;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lk.ac.vau.elms.Model.Permission;
+import lk.ac.vau.elms.Repo.PremissionRepo;
+
+@RestController
+@RequestMapping("/permission")
 public class PermissionController {
 	
+	@Autowired
+	private PremissionRepo repo;
 	
+	//http://localhost:8080/permission
+	public List <Permission> getAllPermissions(){
+		return repo.findAll();
+	}
+		
 }
